6e2a8040ab07a55a0f2da94e0fc12c7fb4c72d92
Osaamaa <osamakhidr1@gmail.com>
2024-01-06 02:20:19 +0200

    till episode 43

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,17 +12,15 @@ class PostController extends Controller
     public function index()
     {
 
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search','category']))->get(),
-            'categories' => Category::all(),
-            'currentCategory'=>  Category::where('slug',request('category'))->first()
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search','category','author']))->get(),
 
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post,
             'categories' => Category::all(),
 
--- routes/web.php
+++ routes/web.php
@@ -6,34 +6,7 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
 Route::get('/', [PostController::class,'index'])->name('home');
-
 Route::get('/posts/{post:slug}',[PostController::class,'show']);
 
-//Route::get('categories/{category:slug}', function (\App\Models\Category $category) {//slug is an attribute in the database dont forget that
-//    return view('posts', [
-//        'posts' => $category->posts,
-//        'currentCategory' => $category,
-//        'categories' => Category::all(),
-//
-//    ]);
-//})->name('category');
-Route::get('authors/{author:username}', function (User $author) {//slug is an attribute in the database dont forget that
-    return view('posts', [
-        'posts' => $author->posts
-    ]);
-});
